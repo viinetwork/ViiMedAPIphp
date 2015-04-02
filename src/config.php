f006cc971ab99f011563bab22b121a9a069299b3
AaronBullard <abullard@theviinetwork.com>
2015-04-02 12:03:27 -0400

    changed config to gwn-demo

--- src/config.php
+++ src/config.php
@@ -3,7 +3,7 @@
 return [
 	'base_urls' => [
 		'authservice' => 'https://gwn-demo.viinetwork.net/',
-		'globaluser' => 'http://viiidservice.app:80/'
-		// 'globaluser' => 'https://gwn-demo.viinetwork.net/'
+		// 'globaluser' => 'http://viiidservice.app:80/'
+		'globaluser' => 'https://gwn-demo.viinetwork.net/'
 	]
 ];
