2e5a9298e88272485920694ac0cafe5a64b2bdda
Ben Matics <bmatics@viimed.com>
2015-10-07 18:44:56 -0400

    wrong class name

--- src/Interfaces/IdentityInterface.php
+++ src/Interfaces/IdentityInterface.php
@@ -1,6 +1,6 @@
 <?php namespace Viimed\PhpApi\Interfaces;
 
-interface SchemaInterface
+interface IdentityInterface
 {
     public function registerIdentity($system_name, $system_id, Array $traits);
     public function findIdentity($system_name, $system_id);
